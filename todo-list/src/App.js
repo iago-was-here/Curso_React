d3fcec880a4d8081d76770976677464f071e322a
Iago Ananias Silva <iago.ananias@bmstecnologia.com.br>
2023-07-10 16:20:58 -0300

    the task list has been added to the app component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,6 +1,70 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { MdDelete } from 'react-icons/md';
 import './App.css';
 
-const App = () => <h1 className="title">App</h1>
+const App = () => {
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const initialTaskList = [
+    { id: 1, description: 'Estudar React', checked: false },
+    { id: 2, description: 'Estudar Ingles', checked: true },
+    { id: 3, description: 'Tocar guitarra', checked: false },
+  ];
+
+  const [taskList] = useState(initialTaskList);
+  const [task, setTask] = useState('');
+
+  const onChange = (event) => {
+    setTask(event.target.value);
+  };
+
+  const erase = () => {
+    setTask('');
+  };
+
+  const submit = () => {
+    console.log(task);
+    erase();
+  }
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
+      submit();
+      return;
+    }
+
+    if (event.which === ESCAPE_KEY) {
+      erase();
+    }
+  };
+
+  return (
+    <section id='app' className='container'>
+      <header>
+        <h1 className='title'>Todo-List</h1>
+      </header>
+      <section className='main'>
+        <input
+          className='new-task'
+          placeholder='O que precisa ser feito ?'
+          value={task} onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+        <ul className='task-list'>
+          {
+            taskList.map((task) => (
+              <li key={task.id.toString()}>
+                <span className='task'>{task.description}</span>
+                <button className='remove' type='button'>
+                  <MdDelete size={28} />
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+      </section>
+    </section>
+  );
+}
 
 export default App;
